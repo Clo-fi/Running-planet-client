7f93f700f8cd5a4b8e3375376aee4167ab5cfdaf
mjmj0217 <mjmj0217@naver.com>
2024-05-13 15:28:51 +0900

    feat: tanstack devtools 추가

---
+++ src/libs/tanstack/TanstackProvider.tsx
@@ -0,0 +1,16 @@
+import { PropsWithChildren } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient(); //TODO 설정 수정
+
+const TanstackProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+export default TanstackProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/index.scss";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient(); // TODO: 설정 수정하기
+import TanstackProvider from "./libs/tanstack/TanstackProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <TanstackProvider>
       <App />
-    </QueryClientProvider>
+    </TanstackProvider>
   </React.StrictMode>
 );
