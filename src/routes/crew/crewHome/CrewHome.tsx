8a50676033946b75fe3037d9eec8206d9bb5536c
KoreanCow <vue-cli@kakao.com>
2024-05-16 21:29:43 +0900

    feat: 바텀시트 구현

--- src/routes/crew/crewHome/CrewHome.tsx
+++ src/routes/crew/crewHome/CrewHome.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 import styles from './CrewHome.module.scss';
 import CrewIntroduction from './components/CrewIntroduction';
+import useMeasure from "react-use-measure";
 import CrewPosts from './components/CrewPosts';
 
 const CrewHome = () => {
+  const [viewportRef, { height: viewportHeight }] = useMeasure();
+
   return (
-    <div className={styles.crew__main_container}>
+    <div className={styles.crew__main_container} ref={viewportRef}>
       <CrewIntroduction />
-      <CrewPosts />
+      <CrewPosts viewport={`${viewportHeight}px`} />
     </div>
   )
 }
