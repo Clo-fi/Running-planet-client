878af685e39eef1c8e787d98be009fa26ba42bb7
KoreanCow <vue-cli@kakao.com>
2024-05-16 12:34:10 +0900

    feat: 프로그레시브 웹앱 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: {
+        name: 'Running_Planet',
+        short_name: 'RunPle',
+        start_url: '/',
+        scope: ".",
+        lang: 'ko-KR',
+        display: 'fullscreen',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+      }
+    })
+  ]
 })
