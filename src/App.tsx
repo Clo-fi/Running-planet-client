3100b7eed832da5c6f55581dd5d688d15e16a961
KoreanCow <vue-cli@kakao.com>
2024-05-16 21:29:21 +0900

    refactor: 라우팅 경로 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,12 @@ import CrewHome from './routes/crew/crewHome/CrewHome';
 function App() {
   return (
     <Routes>
-      <Route path='/crew-search' element={<CrewSearchPage />} />
-      <Route path='/crew/:id' element={<CrewHome />} />
       <Route path="/login" element={<Login />}></Route>
       <Route element={<LayoutWithFooter />}>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
         <Route path='/crew-search' element={<CrewSearchPage />} />
+        <Route path='/crew/:id' element={<CrewHome />} />
       </Route>
       <Route path="/running" element={<RunningPage />} />
       <Route path="/running-complete" element={<RunningCompletePage />} />
