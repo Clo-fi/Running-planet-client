4505d710088d105b1ad80b5ff4005b0a1ff92174
KoreanCow <vue-cli@kakao.com>
2024-05-16 18:54:26 +0900

    feat: 크루페이지 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import CrewSearchPage from './routes/crew/crewSearch/CrewSearchPage';
 import RunningPage from "./routes/running/RunningPage";
 import RunningCompletePage from "./routes/running-complete/RunningCompletePage";
+import CrewHome from './routes/crew/crewHome/CrewHome';
 
 function App() {
   return (
     <Routes>
       <Route path='/crew-search' element={<CrewSearchPage />} />
+      <Route path='/crew/:id' element={<CrewHome />} />
       <Route path="/running" element={<RunningPage />} />
       <Route path="/running-complete" element={<RunningCompletePage />} />
     </Routes>
