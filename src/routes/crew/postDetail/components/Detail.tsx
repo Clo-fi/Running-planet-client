9ccb5e8e9a15024ff82467c1996164e19dd0105e
KoreanCow <vue-cli@kakao.com>
2024-05-20 12:40:06 +0900

    feat: 게시글 상세 조회 페이지 퍼블리싱

--- src/routes/crew/postDetail/components/Detail.tsx
+++ src/routes/crew/postDetail/components/Detail.tsx
@@ -6,7 +6,7 @@ const dummyPost = {
   "author": "몌린",
   "authorId": 1,
   "content": "글 내용입니다.",
-  "likeCnt": 2,
+  "likeCnt": 3,
   "writtenDate": "2024-05-08",
   "imgList": [
     {
@@ -17,49 +17,52 @@ const dummyPost = {
       "id": 2,
       "img": "imgURL"
     }
+  ],
+  "comments": [
+    {
+      "author": "한우혁",
+      "content": "댓글입니다",
+      "createdDate": "2024-04-04 23:15:27"
+    },
+    {
+      "author": "김용빈",
+      "content": "댓글입니다2",
+      "createdDate": "2024-04-04 23:15:27"
+    },
   ]
 }
-
 const Detail = () => {
-  const formatDate = (dateString: string) => {
-    const year = dateString.slice(0, 4);
-    const month = dateString.slice(4, 6);
-    const day = dateString.slice(6, 8);
-    return `${year}.${month}.${day}`;
-  };
-
-
   return (
     <div className={styles.detail__container}>
-      <div>
+      <div className={styles.detail__backspace}>
         <img src='/src/assets/icons/Expand_left.png' alt='backSpaceImg' />
       </div>
-      <div>
-        <p>{dummyPost.title} <span>{dummyPost.author}</span></p>
+      <div className={styles.detail__content_container}>
+        <p className={styles.detail__title}>{dummyPost.title} <span className={styles.detail__author}>{dummyPost.author}</span></p>
         {dummyPost.imgList && dummyPost.imgList.length > 0 && (
-          <div>
+          <>
             {dummyPost.imgList.map((image, index) => (
               <img key={index} src={image.img} alt={`img_${index}`} />
             ))}
-          </div>
+          </>
         )}
         <span>
           {dummyPost.content}
         </span>
       </div>
-      <div>
+      <div className={styles.detail__info}>
         <span>
-          {formatDate(dummyPost.writtenDate)}
+          {dummyPost.writtenDate}
         </span>
-        <div>
-          <div>
+        <div className={styles.detail__info_like_comment}>
+          <>
             <img src="/src/assets/icons/Favorite.png" alt="" />
             <span>{dummyPost.likeCnt}</span>
-          </div>
-          <div>
+          </>
+          <>
             <img src="/src/assets/icons/Bookmark.png" alt="" />
-            <span>ㅇㅈㅁㅇ</span>
-          </div>
+            <span>{dummyPost.comments.length}</span>
+          </>
         </div>
       </div>
     </div>
