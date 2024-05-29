83aa01c46df5c0e300b83412dcbd81e2769e34ba
WooHyuck Han <vue-cli@kakao.com>
2024-05-29 10:48:02 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -41,15 +41,15 @@ export default defineConfig({
             'src': '/logo/android-chrome-192x192.png',
             'sizes': '192x192',
             'type': 'image/png',
-            'purpose': 'maskable'
+            'purpose': 'any maskable'
           },
           {
             'src': '/logo/android-chrome-512x512.png',
             'sizes': '512x512',
             'type': 'image/png',
+            'purpose': 'any'
           },
-        ]
-      }
+        ]      }
     })
   ],
   optimizeDeps: {
