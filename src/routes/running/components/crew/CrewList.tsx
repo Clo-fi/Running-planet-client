f579a93699d0d7ec3c17819d111018161af7db9b
KoreanCow <vue-cli@kakao.com>
2024-05-30 15:04:07 +0900

    feat: 소켓 구문 추가

--- src/routes/running/components/crew/CrewList.tsx
+++ src/routes/running/components/crew/CrewList.tsx
@@ -3,8 +3,8 @@ import styles from "./CrewList.module.scss";
 const CrewList = () => {
   return (
     <section className={styles.container}>
-      {[1, 2, 3, 4, 6, 73, 5, 2, 5, 2, 5, 4, 6, 343].map(() => (
-        <Member isOnline={true} isRun={false} nickname={""} runDistance={0} />
+      {[1, 2, 3, 4, 6, 73, 5, 2, 5, 2, 5, 4, 6, 343].map((_, index) => (
+        <Member key={index} isOnline={true} isRun={false} nickname={""} runDistance={0} />
       ))}
     </section>
   );
