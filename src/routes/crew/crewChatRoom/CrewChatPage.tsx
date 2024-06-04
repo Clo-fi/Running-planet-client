422f5dd6cd65c0340fc2a9b3cfc3797776586b18
KoreanCow <vue-cli@kakao.com>
2024-06-04 17:22:05 +0900

    feat: 크루 채팅방 라우팅

---
+++ src/routes/crew/crewChatRoom/CrewChatPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const CrewChatPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button onClick={() => navigate(-1)}>ㄷ두ㅣ로가기 테스트</button>
+    </div>
+  )
+}
+
+export default CrewChatPage
