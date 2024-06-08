bb0e91e15b85bd612abaf337dde41b460f25ddba
gimye <gimyedev@gmail.com>
2024-06-09 01:25:54 +0900

    refactor: mission 리스트 가져오는 부분 수정

--- src/routes/profile/Profile.tsx
+++ src/routes/profile/Profile.tsx
@@ -3,13 +3,14 @@ import UserCalendar from "../../components/common/calendar/UserCalendar";
 import { useNavigate } from "react-router-dom";
 import { useUserStore } from '../../stores/userStore';
 import { useEffect } from 'react';
-import { useMissionList } from "../crew/crewHome/components/hooks/useMissionList";
+import { MissionList } from "../../types/user/mission";
+// import { useMissionList } from "../crew/crewHome/components/hooks/useMissionList";
 
 const Profile = () => {
   const nav = useNavigate();
   const user = useUserStore((state) => state.user);
 
-  const { data: missionList, isError: isMissionError, isLoading: isMissionLoading } = useMissionList(user?.myCrewId as number);
+  // const { data: missionList, isError: isMissionError, isLoading: isMissionLoading } = useMissionList(user?.myCrewId as number);
 
   const handleSettting = () => {
     nav('/setting');
@@ -18,6 +19,22 @@ const Profile = () => {
   const handleEdit = () => {
     nav('/profile/edit');
   }
+
+  // 화면 확인을 위한 예시 데이터
+  const missionList: MissionList = {
+    missions: [
+      {
+        missionId: 1,
+        missionConent: "3KM 달리기",
+        missonProgress: 51
+      },
+      {
+        missionId: 2,
+        missionConent: "45분 달리기",
+        missonProgress: 87
+      }
+    ]
+  };
 
   // 유저 정보 로딩 안 되어 있으면 홈으로 돌아가서 다시 받아오도록
   useEffect(() => {
@@ -32,14 +49,14 @@ const Profile = () => {
   }
 
   // 로딩 중일 때
-  if (isMissionLoading) {
-    return <div>Loading...</div>;
-  }
+  // if (isMissionLoading) {
+  //   return <div>Loading...</div>;
+  // }
 
-  // 에러가 발생했을 때
-  if (isMissionError) {
-    return <div>Error: 미션 데이터를 불러오는 중 오류가 발생했습니다.</div>;
-  }
+  // // 에러가 발생했을 때
+  // if (isMissionError) {
+  //   return <div>Error: 미션 데이터를 불러오는 중 오류가 발생했습니다.</div>;
+  // }
 
   // 데이터가 성공적으로 로드되었을 때
   return (
@@ -76,12 +93,18 @@ const Profile = () => {
           <div className={styles.info_content}>{user.totalDistance}KM</div>
         </div>
         <div className={styles.info_missions}>
-          <div className={styles.info_type}>나의 운동</div>
+          <div className={`${styles.info_type} ${styles['left-align']}`}>나의 운동</div>
           <div className={styles.info_mission}>
             {missionList?.missions.map((mission) => (
               <div key={mission.missionId}>
-                <div>미션 내용: {mission.missionConent}</div>
-                <div>진행 상황: {mission.missonProgress}%</div>
+                <div className={styles.mission_content}>
+                  <div className={styles.mission_dot}></div>
+                  <div className={styles.mission_title}>{mission.missionConent}</div>
+                  <div className={styles.misson_percent}>{mission.missonProgress}%</div>
+                </div>
+                <div className={styles.progress}>
+                  {/* 여기 안에 progress bar 컴포넌트 만들어서 넣기 */}
+                </div>
               </div>
             ))}
           </div>
