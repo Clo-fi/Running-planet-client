ec86763ac9e015ffa88f8991f2098505f898209e
KoreanCow <vue-cli@kakao.com>
2024-06-10 16:23:27 +0900

    feat: 크루 채팅방

--- src/routes/crew/crewChatRoom/CrewChatPage.tsx
+++ src/routes/crew/crewChatRoom/CrewChatPage.tsx
@@ -1,10 +1,81 @@
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import styles from './CrewChatPage.module.scss'
+import ChatList from './components/ChatList';
+import { useEffect, useState } from 'react';
+import { useWebSocket } from '../../../libs/stomp/useWebSocket';
+import { useUserStore } from '../../../stores/userStore';
+import { StompSubscription } from "@stomp/stompjs";
+
+interface ChatInfo {
+  from: string;
+  message: string;
+  time: Date;
+}
 
 const CrewChatPage = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const { crewId } = useParams();
+  const user = useUserStore((state) => state.user)
+  const [message, setMessage] = useState<string>('');
+  const [chatList, setChatList] = useState<ChatInfo[]>([]);
+
+  const socketClient = useWebSocket();
+  const { data } = location.state;
+
+  useEffect(() => {
+    if (!socketClient || !crewId) return;
+    let subscription: StompSubscription;
+
+    socketClient.onConnect = () => {
+      console.log('채팅 소켓 연결');
+
+      subscription = socketClient.subscribe(
+        `/sub/crew/${crewId}/chat`,
+        (message) => {
+          console.log(message);
+
+          const chatMessage: ChatInfo = JSON.parse(message.body);
+          setChatList((prev) => [...prev, chatMessage]);
+          // setChatList((prev) => [...prev, data])
+        }
+      )
+      return () => {
+        if (socketClient.connected && subscription) {
+          subscription.unsubscribe();
+        }
+      }
+    }
+
+  }, [socketClient, crewId])
+
+  const sendMessageHandle = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    if (!socketClient) return;
+
+    socketClient.publish({
+      destination: `/pub/crew/${crewId}/chat`,
+      body: JSON.stringify({ from: user?.nickname, message: message })
+    });
+    setMessage('');
+  }
+  console.log(data);
   return (
-    <div>
-      <button onClick={() => navigate(-1)}>ㄷ두ㅣ로가기 테스트</button>
+    <div className={styles.main_container}>
+      <div className={styles.top}>
+        <img className={styles.top_backspace} src='/icons/Expand_left.png' alt='backSpaceImg' onClick={() => navigate(-1)} />
+        <p>{data.crewName}</p>
+      </div>
+      <ChatList chatList={chatList} />
+      <div className={styles.input_container}>
+        <form className={styles.chat_form} onSubmit={sendMessageHandle}>
+          <input type='text' value={message} onChange={(e) => setMessage(e.target.value)} className={styles.chat_input} placeholder='채팅을 입력해 주세요' />
+          <button type="submit">
+            <img src="/icons/Send_black.png" alt="Send" />
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
---
+++ src/routes/crew/crewChatRoom/components/ChatList.tsx
@@ -0,0 +1,102 @@
+import { useUserStore } from '../../../../stores/userStore';
+import styles from './ChatList.module.scss'
+
+interface Props {
+  chatList: { from: string, message: string }[]
+}
+
+const dummychat = [
+  {
+    from: '용빈',
+    message: '하이하이',
+    // time: new Date
+  },
+  {
+    from: '한우혁',
+    message: '하이하이',
+    // time: '2002'
+  },
+  {
+    from: '한우혁',
+    message: '하이하이'
+  },
+  {
+    from: '용빈',
+    message: '하이하이'
+  },
+  {
+    from: '용빈',
+    message: '하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이하이',
+    // time: new Date
+  },
+  {
+    from: '한우혁',
+    message: '하이하이',
+    // time: '2002'
+  },
+  {
+    from: '한우혁',
+    message: '하이하이'
+  },
+  {
+    from: '용빈',
+    message: '하이하이'
+  }, {
+    from: '용빈',
+    message: '하이하이',
+    // time: new Date
+  },
+  {
+    from: '한우혁',
+    message: '하이하이',
+    // time: '2002'
+  },
+  {
+    from: '한우혁',
+    message: '하이하이'
+  },
+  {
+    from: '용빈',
+    message: '하이하이'
+  }
+  , {
+    from: '용빈',
+    message: '하이하이',
+    // time: new Date
+  },
+  {
+    from: '한우혁',
+    message: '하이하이',
+    // time: '2002'
+  },
+  {
+    from: '한우혁',
+    message: '하이하이'
+  },
+  {
+    from: '용빈',
+    message: '하이하이'
+  }
+]
+const ChatList: React.FC<Props> = (chatList) => {
+  console.log(chatList);
+  const user = useUserStore((state) => state.user)
+  return (
+    <div className={styles.chat_container}>
+      {dummychat && dummychat.length > 0 && dummychat.map((chat, index) => (
+        <div key={index} className={chat.from === user?.nickname ? styles.chat_my_box : ''}>
+          {index > 0 && dummychat[index - 1].from === chat.from ? null : (
+            <p className={chat.from === user?.nickname ? styles.chat_my_name : styles.chat_username}>
+              {chat.from !== user?.nickname ? chat.from : null}
+            </p>
+          )}
+          <p className={chat.from === user?.nickname ? styles.chat_my_message : styles.chat_message}>
+            <span>{chat.message}</span>
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ChatList
