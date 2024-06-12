c9c56cdf080a9682a614c4be12bb441c20da3c31
KoreanCow <vue-cli@kakao.com>
2024-06-12 16:11:49 +0900

    feat: 댓글, 좋아요 버튼 업데이트 시 리페치

--- src/routes/crew/postDetail/components/Detail.tsx
+++ src/routes/crew/postDetail/components/Detail.tsx
@@ -4,53 +4,30 @@ import { Post } from '../../../../types/crew/crewPost';
 import { useNavigate, useParams } from 'react-router-dom';
 import parse from 'html-react-parser';
 import instance from '../../../../libs/api/axios';
-// const dummyPost = {
-//   "title": "글 제목입니다.",
-//   "author": "몌린",
-//   "authorId": 1,
-//   "content": "글 내용입니다.",
-//   "likeCnt": 3,
-//   "writtenDate": "2024-05-08",
-//   "imgList": [
-//     {
-//       "id": 1,
-//       "img": "imgURL"
-//     },
-//     {
-//       "id": 2,
-//       "img": "imgURL"
-//     }
-//   ],
-//   "comments": [
-//     {
-//       "author": "한우혁",
-//       "content": "댓글입니다",
-//       "createdDate": "2024-04-04 23:15:27"
-//     },
-//     {
-//       "author": "김용빈",
-//       "content": "댓글입니다2",
-//       "createdDate": "2024-04-04 23:15:27"
-//     },
-//   ]
-// }
+
 interface DetailProps {
   data: Post | undefined;
-  isLoading: boolean
+  isLoading: boolean;
+  isLiked: boolean | undefined;
+  authorId: number | undefined;
+
+  onNewComment: () => void;
 }
 
-const Detail: React.FC<DetailProps> = ({ data, isLoading }) => {
-
+const Detail: React.FC<DetailProps> = ({ data, isLoading, isLiked, authorId, onNewComment }) => {
+  console.log(authorId);
   const { crewId, boardId } = useParams();
 
   const likeHandler = async () => {
     try {
-      const response = await instance.post(`/crew/${crewId}/board/${boardId}/like`)
+      const response = await instance.post(`/crew/${crewId}/board/${boardId}/like`);
       console.log(response);
+      onNewComment();
     } catch (error) {
       console.error(error);
     }
   }
+
   const navigate = useNavigate();
   if (isLoading) {
     return <div className={styles.detail__container}>Loading...</div>;
@@ -65,7 +42,7 @@ const Detail: React.FC<DetailProps> = ({ data, isLoading }) => {
       <div className={styles.detail__top}>
         <img className={styles.detail__backspace} src='/icons/Expand_left.png' alt='backSpaceImg' onClick={() => navigate(-1)} />
         <p className={styles.detail__title}>{data.title}</p>
-        <img className={styles.detail__edit} src='/icons/Edit.png' alt='editIgm' />
+        <img className={styles.detail__edit} src='/icons/Edit.png' alt='editImg' />
       </div>
       <div className={styles.detail__content_container}>
         {data.imageList && data.imageList.length > 0 ? (
@@ -88,7 +65,7 @@ const Detail: React.FC<DetailProps> = ({ data, isLoading }) => {
             {data.writtenDate}
           </span>
           <div className={styles.detail__info_like_comment}>
-            <img src="/icons/Favorite.png" alt="likeImg" onClick={likeHandler} />
+            <img src={isLiked ? '/icons/Favorite_full.png' : `/icons/Favorite.png`} alt="likeImg" onClick={likeHandler} />
             <span>{data.likeCnt}</span>
           </div>
         </div>
@@ -97,8 +74,7 @@ const Detail: React.FC<DetailProps> = ({ data, isLoading }) => {
         </span>
       </div>
       <p className={styles.detail__author}>{data.author}</p>
-
     </div>
   );
 }
-export default Detail
+export default Detail;
