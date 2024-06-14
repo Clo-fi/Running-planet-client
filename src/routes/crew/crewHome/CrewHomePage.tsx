2c23ccc0f81016a1993423a94c4dc877032deaf4
KoreanCow <vue-cli@kakao.com>
2024-06-14 18:31:32 +0900

    feat: 미션 연동

--- src/routes/crew/crewHome/CrewHomePage.tsx
+++ src/routes/crew/crewHome/CrewHomePage.tsx
@@ -7,60 +7,51 @@ import { MissionList } from '../../../types/user/mission';
 import { useUserStore } from '../../../stores/userStore';
 import { useEffect, useState } from 'react';
 import { useCrewDetail } from './components/hooks/useCrewDetail';
-// import { useMissionList } from './components/hooks/useMissionList';
+import { useMissionList } from './components/hooks/useMissionList';
 
-const dummyMission: MissionList = {
-  missions: [
-    {
-      missionId: 1,
-      missionConent: "3KM 달리기",
-      missonProgress: 51
-    },
-    {
-      missionId: 2,
-      missionConent: "45분 달리기",
-      missonProgress: 87
-    }
-  ]
-};
 const CrewHomePage = () => {
   const [viewportRef, { height: viewportHeight }] = useMeasure();
   const user = useUserStore((state) => state.user);
 
   const [crewData, setCrewData] = useState<CrewDetail | null>(null);
-  // const [missionData, setMissionData] = useState<MissionList[]>([]);
-  // const { data: missionList, isError: isMissionError, isLoading: isMissionLoading } = useMissionList(user?.myCrewId as number);
+  const [missionData, setMissionData] = useState<MissionList | null>(null);
+  const { data: missionList, isError: isMissionError, isLoading: isMissionLoading } = useMissionList(user?.myCrewId as number);
   const { data: crewDetail, isError: isCrewError, isLoading: isCrewLoading } = useCrewDetail(user?.myCrewId as number);
 
   useEffect(() => {
     if (!isCrewLoading && !isCrewError && crewDetail) {
       setCrewData(crewDetail);
-      console.log(crewDetail);
+      console.log('크루 상세 정보:', crewDetail);
     }
   }, [crewDetail, isCrewLoading, isCrewError]);
 
-  // useEffect(() => {
-  //   if (!isMissionLoading && !isMissionError && missionList) {
-  //     setMissionData(missionList);
-  //   }
-  // }, [missionList, isMissionLoading, isMissionError]);
+  useEffect(() => {
+    if (!isMissionLoading && !isMissionError && missionList) {
+      setMissionData(missionList);
+      console.log('미션 리스트:', missionList);
+    }
+  }, [missionList, isMissionLoading, isMissionError]);
 
-  if (isCrewLoading /*|| isMissionLoading*/) {
+  useEffect(() => {
+    console.log('현재 미션 데이터 상태:', missionData);
+  }, [missionData]);
+
+  if (isCrewLoading || isMissionLoading) {
     return <p>기다려주세요...</p>;
   }
 
-  if (isCrewError /*|| isMissionError*/) {
+  if (isCrewError || isMissionError) {
     return <p>데이터를 불러오는데 실패했습니다. 다시 시도해주세요.</p>;
   }
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <div className={styles.crew__main_container} ref={viewportRef}>
-        <CrewIntroduction data={crewData} missions={dummyMission} />
+        <CrewIntroduction data={crewData} missions={missionData} />
         <CrewPosts data={crewData} viewport={`${viewportHeight}px`} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CrewHomePage;
