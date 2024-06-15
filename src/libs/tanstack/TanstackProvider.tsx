02f49a036fcb36b7d37955fd9e71a7a9fff05595
KoreanCow <vue-cli@kakao.com>
2024-06-15 23:20:38 +0900

    feat: 리액트쿼리 클라이언트 설정 추가

--- src/libs/tanstack/TanstackProvider.tsx
+++ src/libs/tanstack/TanstackProvider.tsx
@@ -3,14 +3,15 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
-  // defaultOptions: {
-  //   queries: {
-  //     throwOnError: true,
-  //   },
-  //   mutations: {
-  //     throwOnError: true,
-  //   },
-  // },
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+      refetchOnWindowFocus: false,
+    },
+    mutations: {
+      throwOnError: true,
+    },
+  },
 }); //TODO 나중에 위 설정 키기
 
 const TanstackProvider = ({ children }: PropsWithChildren) => {
